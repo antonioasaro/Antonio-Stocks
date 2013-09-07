16f57c7b29238aeacdb48854668df56ed76a0701
mangledlftthumb <mangledlftthumb@gmail.com>
2013-09-07 12:17:31 -0400

    Working version

---
+++ index.php
@@ -0,0 +1,48 @@
+<?PHP
+
+$stock = "AMD";
+//Obtain Quote Info
+$quote = file_get_contents('http://dev.markitondemand.com/Api/Quote/json?symbol='.$stock);
+
+//decode JSON data
+$json_output = json_decode(utf8_decode($quote));
+if (!$json_output) {
+	die();
+}
+
+$result = array();
+$symb = $json_output->Data->Symbol;
+$last = $json_output->Data->LastPrice;
+$perc = $json_output->Data->ChangePercent;
+$date = $json_output->Data->Timestamp;
+
+$result[0] = array('S', $symb);
+$result[1] = array('P', round($last, 2));
+$result[2] = array('C', round($perc, 2));
+// $result[3] = array('TS', $date);
+
+print json_encode($result);
+
+/*
+stdClass Object
+(
+    [Data] => stdClass Object
+        (
+            [Status] => SUCCESS
+            [Name] => Facebook Inc
+            [Symbol] => FB
+            [LastPrice] => 31.91
+            [Change] => -1.12
+            [ChangePercent] => -3.3908567968514
+            [Timestamp] => Fri May 25 16:00:05 UTC-04:00 2012
+            [MarketCap] => 20214729720
+            [Volume] => 37189630
+            [ChangeYTD] => 0
+            [ChangePercentYTD] => 0
+            [High] => 32.95
+            [Low] => 31.11
+            [Open] => 32.9
+        )
+
+)*/
+?>
