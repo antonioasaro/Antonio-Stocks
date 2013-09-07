27ce43fb7f21789d625d50c8a7bf2d10fb13c8f5
antonioasaro <antonioasaro@users.noreply.github.com>
2013-09-07 10:34:07 -0700

    First successful compile

--- src/pblindex.c
+++ src/pblindex.c
@@ -43,8 +43,8 @@ PBL_APP_INFO(HTTP_UUID,
 #define COLUMN2_WIDTH 65
 
 // #error Set values below, should be different, one unique for each request
-#define PBLINDEX_NAMES_COOKIE some-unique-value
-#define PBLINDEX_VALUES_COOKIE some-unique-value
+#define PBLINDEX_COOKIE 9997
+//// #define PBLINDEX_VALUES_COOKIE some-unique-value
 
 Window window;
 TextLayer textLayer[2][NUM_LINES];
@@ -58,66 +58,45 @@ void set_display_fail(char *text) {
         text_layer_set_text(&textLayer[1][i], "");
 }
 
-void request_names() {
+void request_quotes() {
     DictionaryIterator *body;
-#error Set URL below
+//// #error Set URL below
     // http://nnnn/nnnn.names should return something like (not over 76 bytes)
     // {"0":"OMXS30","1":"Dow Jones","2":"Nasdaq","3":"DAX","4":"Nikkei"}
-    if (http_out_get("http://nnnn/nnnn.names", false, PBLINDEX_NAMES_COOKIE, &body) != HTTP_OK ||
+    if (http_out_get("http://antonio.comyr.com", false, PBLINDEX_COOKIE, &body) != HTTP_OK ||
         http_out_send() != HTTP_OK) {
-        set_display_fail("names..");
-    }
-}
-
-void request_values() {
-    DictionaryIterator *body;
-#error Set URL below
-    // http://nnnn/nnnn.values should return something like (not over 76 bytes)
-    // {"0":"1210","1":"15464","2":"3600","3":"8212","4":"14506"}
-    if (http_out_get("http://nnnn/nnnn.values", false, PBLINDEX_VALUES_COOKIE, &body) != HTTP_OK ||
-        http_out_send() != HTTP_OK) {
-        set_display_fail("values..");
+        set_display_fail("quotes ...");
     }
 }
 
 void failed(int32_t cookie, int http_status, void *ctx) {
     if (cookie == 0 ||
-        cookie == PBLINDEX_NAMES_COOKIE ||
-        cookie == PBLINDEX_VALUES_COOKIE) {
+		cookie == PBLINDEX_COOKIE) {
         set_display_fail("failed()");
     }
 }
 
 void success(int32_t cookie, int http_status, DictionaryIterator *dict, void *ctx) {
-    if (cookie != PBLINDEX_NAMES_COOKIE && cookie != PBLINDEX_VALUES_COOKIE)
+    if (cookie != PBLINDEX_COOKIE)
         return;
-    int li = cookie == PBLINDEX_NAMES_COOKIE ? 0 : 1;
     for (int i=0; i<NUM_LINES; i++) {
         Tuple *value = dict_find(dict, i);
         if (value) {
             static char str[2][NUM_LINES][16];
-            strcpy(str[li][i], value->value->cstring);
-            text_layer_set_text(&textLayer[li][i], str[li][i]);
+////            strcpy(str[li][i], value->value->cstring);
+////            text_layer_set_text(&textLayer[li][i], str[li][i]);
         } else {
-            text_layer_set_text(&textLayer[li][i], "-");
+////            text_layer_set_text(&textLayer[li][i], "-");
         }
     }
-    if (!li) {
-        request_values();
-    } else {
-        light_enable_interaction();
-//#if MAKE_SCREENSHOT
-//		http_capture_send(20);
-//#endif
-	}
 }
 
 void reconnect(void *ctx) {
-    request_names();
+    request_quotes();
 }
 
 void init_handler(AppContextRef ctx) {
-    window_init(&window, "pbl-index");
+    window_init(&window, "Antonio Stocks");
     window_set_background_color(&window, GColorBlack);
     window_stack_push(&window, true);
     
@@ -133,8 +112,8 @@ void init_handler(AppContextRef ctx) {
         text_layer_set_text_alignment(&textLayer[1][i], GTextAlignmentRight);
     }
     
-    text_layer_set_text(&textLayer[0][0], "pbl-index");
-    text_layer_set_text(&textLayer[0][1], "by epatel");
+    text_layer_set_text(&textLayer[0][0], "Antonio Stocks");
+    text_layer_set_text(&textLayer[0][1], "Antonio Asaro");
     text_layer_set_text(&textLayer[0][3], "loading...");
     
     for (int i=0; i<NUM_LINES; i++) {
@@ -142,7 +121,7 @@ void init_handler(AppContextRef ctx) {
         layer_add_child(&window.layer, &textLayer[1][i].layer);
     }
     
-    http_set_app_id(PBLINDEX_NAMES_COOKIE);
+    http_set_app_id(PBLINDEX_COOKIE);
     
     http_register_callbacks((HTTPCallbacks){
         .failure = failed,
@@ -154,7 +133,7 @@ void init_handler(AppContextRef ctx) {
 	http_capture_init(ctx);
 #endif
 	
-	request_names();
+	request_quotes();
 }
 
 void pbl_main(void *params) {
