24f968e9e52efa3aceb5c8e4634e613350a52424
antonioasaro <antonio_asaro@hotmail.com>
2013-09-11 23:39:48 -0400

    Fix array troubles in index.php

--- server/index.php
+++ server/index.php
@@ -1,20 +1,20 @@
 <?PHP
 
-$quotes = array();
 if (!isset($_GET['stock1']) || !isset($_GET['stock2']) || !isset($_GET['stock3'])) die();
-$stock = $_GET['stock1'];
-$quote = curl_get('http://dev.markitondemand.com/Api/Quote/json?symbol='.$stock);
-$quotes = array_merge($quotes, process_quotes($quote));
 
-$stock = $_GET['stock2'];
-$quote = curl_get('http://dev.markitondemand.com/Api/Quote/json?symbol='.$stock);
-$quotes = array_merge($quotes, process_quotes($quote));
+$stock1 = $_GET['stock1'];
+$stock2 = $_GET['stock2'];
+$stock3 = $_GET['stock3'];
 
-$stock = $_GET['stock3'];
-$quote = curl_get('http://dev.markitondemand.com/Api/Quote/json?symbol='.$stock);
-$quotes = array_merge($quotes, process_quotes($quote));
+$json1  = curl_get('http://dev.markitondemand.com/Api/Quote/json?symbol='.$stock1);
+$json2  = curl_get('http://dev.markitondemand.com/Api/Quote/json?symbol='.$stock2);
+$json3  = curl_get('http://dev.markitondemand.com/Api/Quote/json?symbol='.$stock3);
 
-print json_encode($quotes);
+$quote1 = process_quotes($json1, 1+0*3);
+$quote2 = process_quotes($json2, 1+1*3);
+$quote3 = process_quotes($json3, 1+2*3);
+
+print json_encode($quote1 + $quote2 + $quote3);
 
 // Obtain Quote Info
 // $quote = file_get_contents('http://dev.markitondemand.com/Api/Quote/json?symbol='.$stock);
@@ -35,19 +35,18 @@ function curl_get($url){
     return $output;
 }
 
-function process_quotes($quote) {
+function process_quotes($quote, $i) {
     $json_output = json_decode(utf8_decode($quote));
     if (!$json_output) die(); 
 
     $symb = $json_output->Data->Symbol;
     $last = $json_output->Data->LastPrice;
     $perc = $json_output->Data->ChangePercent;
-//    $date = $json_output->Data->Timestamp;
 	
     $result = array();
-    $result[1] = $symb;
-    $result[2] = array('I', round($last, 2) * 100);
-    $result[3] = array('I', round($perc, 2) * 100);
+    $result[$i+0] = $symb;
+    $result[$i+1] = array('I', round($last, 2) * 100);
+    $result[$i+2] = array('I', round($perc, 2) * 100);
     return $result;
 }
 
